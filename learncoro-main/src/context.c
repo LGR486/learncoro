f0b6a1e8d212515344154e47d7909d44401c3a1a
LGR486 <2441979122@qq.com>
2025-12-05 16:46:14 +0800

    修复一些bug

---
+++ learncoro-main/src/context.c
@@ -0,0 +1,80 @@
+#include "context.h"
+
+
+// 协程的入口
+static VOID WINAPI fiber_entry(context_t ipParameter) {
+	coroutine_t* co = (coroutine_t*)ipParameter;
+
+	co->func(co->arg);
+
+	co->state = FINISED;// 协程函数在这里结束
+	
+	// 执行主线程
+	current_coroutine = NULL;
+	SwitchToFiber(main_fiber);
+	// 在调度器中直接跳到resume()后,因此一般都在resume()后处理完成的协程
+}
+
+// 创建协程
+coroutine_t* create(void (*func)(void*), void* arg) {
+	// 主线程纤维初始化,可以单独放出来init
+	if (!main_fiber) {
+		main_fiber = ConvertThreadToFiber(NULL);
+		if (!main_fiber) {
+			main_fiber = ConvertThreadToFiberEx(NULL, FIBER_FLAG_FLOAT_SWITCH);
+		}
+	}
+
+	coroutine_t* co = (coroutine_t*)malloc(sizeof(coroutine_t));
+	if (co == NULL) {
+		perror("内存分配失败");
+		return NULL;
+	}
+
+	co->func = func;
+	co->arg = arg;
+	co->state = READY;
+	co->main_fiber = main_fiber;
+	co->next = NULL;
+
+	co->fiber = CreateFiber(0, fiber_entry, co);
+	if (!co->fiber) {
+		co->fiber = CreateFiberEx(0, 0, FIBER_FLAG_FLOAT_SWITCH, fiber_entry, co);
+	}
+	return co;
+}
+
+// resume(co)分配CPU,给协程
+void resume(coroutine_t* co) {
+	if (!co || co->state == FINISED) return;// 在此添加返回告诉调度器FINISED // 应在fiber_entry处理FINISED，这是最快处理
+
+	coroutine_t* prev = current_coroutine;
+	current_coroutine = co;
+	co->state = RUNNING;
+
+	SwitchToFiber(co->fiber);
+
+	current_coroutine = prev;
+	if (co->state == RUNNING) {
+		co->state = READY;
+	}
+}
+
+// yield()让出CPU,给主线程,
+// 不是阻塞,主线程可能会结束
+void yield(void) {
+	if (!current_coroutine) return;
+
+	coroutine_t* co = current_coroutine;
+	current_coroutine = NULL;
+
+	SwitchToFiber(co->main_fiber);
+}
+
+// destroy(co)销毁协程
+void destroy(coroutine_t* co) {
+	if (co->fiber) {
+		DeleteFiber(co->fiber);
+	}
+	free(co);
+}
